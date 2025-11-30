425da5177d05f0e975d81558e882d9f2e7308204
CodeBotDevJu <codebotdevju@gmail.com>
2025-11-30 16:24:46 +0100

    Add Underscore_ channel ID to script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const CHANNEL_IDS = [
     'UCdV6JBe40vqPQOsG4boS5zQ', // TRY
     'UCZU9T1ceaOgwfLRq7OKFU4Q', // Linkin Park
     'UCnxEvD2eagj2VgtkFL5wDHA', // V2F
+    'UCWedHS9qKebauVIK2J7383g', // Underscore_
 ];
 
 const MAX_RESULTS_PER_CHANNEL = 5; // Nombre de vidéos à récupérer par chaîne
